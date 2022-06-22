f4bf20aab30755bf9832e173aa399110b4908b42
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2022-06-22 18:34:17 -0300

    Api faker

--- desafio-10/faker/public/js/client.js
+++ desafio-10/faker/public/js/client.js
@@ -1,6 +1,10 @@
 window.onload = () => {
     const socket = io();
 
+    socket.on("messages", data => {
+        loadMessages(data)
+    })
+
     socket.on("productos", listaProductos => {
         loadProds(listaProductos)
     })
@@ -29,6 +33,32 @@ window.onload = () => {
     socket.emit("guardarNuevoProducto",nuevoProducto)
     })
 
+    //MENSAJES
 
+    function loadMessages(data) {
+        const html = data.map((elem, index) => {
+            return(`
+                <div class="direct-chat-info clearfix">
+                    <span id="chatName" class="direct-chat-name pull-right">${elem.email}</span>
+                    <span id= "chatDate" class="direct-chat-timestamp pull-left">${elem.date}</span>
+                </div>
+                <div id="chatText" class="direct-chat-text">${elem.textoMensaje}</div>
+            `)
+        }).join(" ");
+        document.getElementById('messages').innerHTML = html;
+    }
+
+    document.getElementById('formChat').addEventListener('submit', (e) => {
+        e.preventDefault()
+        agregarMensaje()
+    })
+
+    function agregarMensaje() {
+        const nuevoMensaje = {
+            email: document.getElementById('email').value,
+            textoMensaje: document.getElementById('textoMensaje').value
+        }
+        socket.emit("messegesNew",nuevoMensaje)
+    }
 }
 
