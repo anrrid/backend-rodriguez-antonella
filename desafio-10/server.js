f1d84ed8d9e8ef7511a18362a70b266485f8d16b
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2022-06-28 03:11:00 -0300

    Revert "Normalizr"
    This reverts commit b239bc8d551184f85a5e1c17c5c679ffe49846e8.

--- desafio-10/server.js
+++ desafio-10/server.js
@@ -68,7 +68,7 @@ io.on('connection', async (sockets) => {
         const text = data.message
         const date = data.date
         const hour = data.hour
-        await ContainerMsg.saveMsg(author, text, date, hour)
+        await ContainerMsg.saveMsj(author, text, date, hour)
         console.log(data, 'data-message')
 
         io.sockets.emit('messages', await ContainerMsg.getMsg())
@@ -80,8 +80,7 @@ io.on('connection', async (sockets) => {
 async function getNormalizedMessage() {
     const messages = await ContainerMsg.getMsg()
     const normalized = normalizeMessages ({ id: 'mensajes', messages})
-    const answer = print(normalized)
-    return answer
+    return normalized
 }
 
 async function denormalizedList(normMessages) {
--- desafio-10/src/scriptDB/crearTablas.js
+++ desafio-10/src/scriptDB/crearTablas.js
@@ -1,4 +1,6 @@
 
+const Contenedor = require('../controllers/containerMsg.js')
 const Container = require('../controllers/containerProd.js')
 
 Container.createTable()
+Contenedor.saveMsg()
